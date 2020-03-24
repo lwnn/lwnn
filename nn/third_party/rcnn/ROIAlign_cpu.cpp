060917f88891dbdc3f9183b62994367190d2e4d8
parai.wang <parai@foxmail.com>
2020-03-24 20:58:10 +0800

    make ROI works for lwnn

--- nn/third_party/rcnn/ROIAlign_cpu.cpp
+++ nn/third_party/rcnn/ROIAlign_cpu.cpp
@@ -10,10 +10,10 @@
 // implementation taken from Caffe2
 template <typename T>
 struct PreCalc {
-  int pos1;
-  int pos2;
-  int pos3;
-  int pos4;
+  int x1;
+  int y1;
+  int x2;
+  int y2;
   T w1;
   T w2;
   T w3;
@@ -40,12 +40,10 @@ void pre_calc_for_bilinear_interpolate(
     for (int pw = 0; pw < pooled_width; pw++) {
       for (int iy = 0; iy < iy_upper; iy++) {
         const T yy = roi_start_h + ph * bin_size_h +
-            static_cast<T>(iy + .5f) * bin_size_h /
-                static_cast<T>(roi_bin_grid_h); // e.g., 0.5, 1.5
+            static_cast<T>(iy + .5f) * bin_size_h / static_cast<T>(roi_bin_grid_h); // e.g., 0.5, 1.5
         for (int ix = 0; ix < ix_upper; ix++) {
           const T xx = roi_start_w + pw * bin_size_w +
-              static_cast<T>(ix + .5f) * bin_size_w /
-                  static_cast<T>(roi_bin_grid_w);
+              static_cast<T>(ix + .5f) * bin_size_w / static_cast<T>(roi_bin_grid_w);
 
           T x = xx;
           T y = yy;
@@ -53,10 +51,10 @@ void pre_calc_for_bilinear_interpolate(
           if (y < -1.0 || y > height || x < -1.0 || x > width) {
             // empty
             PreCalc<T> pc;
-            pc.pos1 = 0;
-            pc.pos2 = 0;
-            pc.pos3 = 0;
-            pc.pos4 = 0;
+            pc.x1 = 0;
+            pc.y1 = 0;
+            pc.x2 = 0;
+            pc.y2 = 0;
             pc.w1 = 0;
             pc.w2 = 0;
             pc.w3 = 0;
@@ -99,15 +97,16 @@ void pre_calc_for_bilinear_interpolate(
 
           // save weights and indices
           PreCalc<T> pc;
-          pc.pos1 = y_low * width + x_low;
-          pc.pos2 = y_low * width + x_high;
-          pc.pos3 = y_high * width + x_low;
-          pc.pos4 = y_high * width + x_high;
+          pc.x1 = x_low;
+          pc.y1 = y_low;
+          pc.x2 = x_high;
+          pc.y2 = y_high;
           pc.w1 = w1;
           pc.w2 = w2;
           pc.w3 = w3;
           pc.w4 = w4;
           pre_calc[pre_calc_index] = pc;
+          //NNLOG(NN_DEBUG, (" PC[%d] W=[%f %f %f %f] P=[%d %d %d %d]\n", pre_calc_index, w1, w2, w3, w4, pc.y1, pc.x1, pc.y2, pc.x2));
 
           pre_calc_index += 1;
         }
@@ -130,8 +129,7 @@ void ROIAlignForward_cpu_kernel(
     const T* bottom_rois,
     //int roi_cols,
     T* top_data) {
-  //AT_ASSERT(roi_cols == 4 || roi_cols == 5);
-  int roi_cols = 5;
+  const int roi_cols = 4;
 
   int n_rois = nthreads / channels / pooled_width / pooled_height;
   // (n, c, ph, pw) is an element in the pooled output
@@ -140,23 +138,13 @@ void ROIAlignForward_cpu_kernel(
   for (int n = 0; n < n_rois; n++) {
     int index_n = n * channels * pooled_width * pooled_height;
 
-    // roi could have 4 or 5 columns
     const T* offset_bottom_rois = bottom_rois + n * roi_cols;
-    int roi_batch_ind = 0;
-    if (roi_cols == 5) {
-      roi_batch_ind = offset_bottom_rois[0];
-      offset_bottom_rois++;
-    }
 
     // Do not using rounding; this implementation detail is critical
-    T roi_start_w = offset_bottom_rois[0] * spatial_scale;
-    T roi_start_h = offset_bottom_rois[1] * spatial_scale;
-    T roi_end_w = offset_bottom_rois[2] * spatial_scale;
-    T roi_end_h = offset_bottom_rois[3] * spatial_scale;
-    // T roi_start_w = round(offset_bottom_rois[0] * spatial_scale);
-    // T roi_start_h = round(offset_bottom_rois[1] * spatial_scale);
-    // T roi_end_w = round(offset_bottom_rois[2] * spatial_scale);
-    // T roi_end_h = round(offset_bottom_rois[3] * spatial_scale);
+    T roi_start_h = offset_bottom_rois[0] * spatial_scale * height;
+    T roi_start_w = offset_bottom_rois[1] * spatial_scale * width;
+    T roi_end_h = offset_bottom_rois[2] * spatial_scale * height;
+    T roi_end_w = offset_bottom_rois[3] * spatial_scale * width;
 
     // Force malformed ROIs to be 1x1
     T roi_width = std::max(roi_end_w - roi_start_w, (T)1.);
@@ -173,6 +161,10 @@ void ROIAlignForward_cpu_kernel(
 
     // We do average (integral) pooling inside a bin
     const T count = roi_bin_grid_h * roi_bin_grid_w; // e.g. = 4
+
+    NNLOG(NN_DEBUG, ("  start=(%.1f,%.1f) h,w=(%.1f,%.1f), bins=(%.1f,%.1f), bing=(%d,%d)\n",
+            roi_start_h, roi_start_w, roi_height, roi_width,
+            bin_size_h, bin_size_w, roi_bin_grid_h, roi_bin_grid_w));
 
     // we want to precalculate indices and weights shared by all channels,
     // this is the key point of optimization
@@ -194,30 +186,25 @@ void ROIAlignForward_cpu_kernel(
         pre_calc);
 
       for (int c = 0; c < channels; c++) {
-      int index_n_c = index_n + c * pooled_width * pooled_height;
-      const T* offset_bottom_data =
-          bottom_data + (roi_batch_ind * channels + c) * height * width;
       int pre_calc_index = 0;
 
       for (int ph = 0; ph < pooled_height; ph++) {
         for (int pw = 0; pw < pooled_width; pw++) {
-          int index = index_n_c + ph * pooled_width + pw;
-
           T output_val = 0.;
           for (int iy = 0; iy < roi_bin_grid_h; iy++) {
             for (int ix = 0; ix < roi_bin_grid_w; ix++) {
               PreCalc<T> pc = pre_calc[pre_calc_index];
-              output_val += pc.w1 * offset_bottom_data[pc.pos1] +
-                  pc.w2 * offset_bottom_data[pc.pos2] +
-                  pc.w3 * offset_bottom_data[pc.pos3] +
-                  pc.w4 * offset_bottom_data[pc.pos4];
-
+              output_val += \
+                  pc.w1 * bottom_data[(pc.y1*width+pc.x1)*channels+c] + /* pc.pos1 = y_low * width + x_low; */
+                  pc.w2 * bottom_data[(pc.y1*width+pc.x2)*channels+c] + /* pc.pos2 = y_low * width + x_high; */
+                  pc.w3 * bottom_data[(pc.y2*width+pc.x1)*channels+c] + /* pc.pos3 = y_high * width + x_low; */
+                  pc.w4 * bottom_data[(pc.y2*width+pc.x2)*channels+c];  /* pc.pos4 = y_high * width + x_high; */
               pre_calc_index += 1;
             }
           }
           output_val /= count;
 
-          top_data[index] = output_val;
+          top_data[(ph*pooled_width+pw)*channels+c] = output_val;
         } // for pw
       } // for ph
     } // for c
@@ -232,7 +219,7 @@ int calc_roi_level(float y1, float x1, float y2, float x2, float image_area, int
     level = (x2-x1)*(y2-y1);
     level = sqrt(level) / (224.0 / sqrt(image_area));
     level = log(level) / log(2.0);
-    level = std::min((double)n_features, std::max(0.0, 2.0+std::round(level)));
+    level = std::min((double)n_features-1, std::max(0.0, 2.0+std::round(level)));
     roi_level = level;
   }
 
@@ -256,7 +243,6 @@ extern "C" int ROIAlign_forward_cpu(const nn_t* nn, const layer_t* layer)
   float* meta = (float*)meta_context->out[0];
   float image_height = meta[2];
   float image_width = meta[3];
-  float spatial_scale = meta[8];
   float image_area = image_height*image_width;
   float* boxes = (float*)roi_context->out[0];
   float* feature;
@@ -268,8 +254,9 @@ extern "C" int ROIAlign_forward_cpu(const nn_t* nn, const layer_t* layer)
 
   context->nhwc.N = num_rois;
 
-  NNLOG(NN_DEBUG, ("execute %s [%d %d %d %d]: spatial_scale=%f, image_shape=[%d %d], %d features\n",
-       layer->name, L_SHAPES(layer), spatial_scale, (int)image_height, (int)image_width, n_features));
+  NNLOG(NN_DEBUG, ("execute %s [%d %d %d %d]: image_shape=[%d %d], pool=[%d %d], %d features\n",
+       layer->name, L_SHAPES(layer), (int)image_height, (int)image_width,
+       pooled_height, pooled_width, n_features));
 
   for(i=0; i<num_rois; i++) {
     y1 = boxes[4*i+0];
@@ -277,14 +264,14 @@ extern "C" int ROIAlign_forward_cpu(const nn_t* nn, const layer_t* layer)
     y2 = boxes[4*i+2];
     x2 = boxes[4*i+3];
     roi_level = calc_roi_level(y1,x1,y2,x2,image_area, n_features);
-    NNLOG(NN_DEBUG, ("  ROI @[%d %d %d %d] from feature %d [%d %d %d %d]\n",
+    NNLOG(NN_DEBUG, (" ROI @[%d %d %d %d] from feature %d [%d %d %d %d]\n",
                     (int)(y1*image_height), (int)(x1*image_width),
                     (int)(y2*image_height), (int)(x2*image_width), roi_level,
-                    L_SHAPES(features[roi_level])));
+                    L_SHAPES(features[roi_level>0?roi_level:0])));
     if(roi_level >= 0) {
       feature_context = features[roi_level]->C->context;
       feature = (float*)feature_context->out[0];
-      ROIAlignForward_cpu_kernel(roi_size, feature, spatial_scale, channels,
+      ROIAlignForward_cpu_kernel(roi_size, feature, 1.0f, channels,
               feature_context->nhwc.H, feature_context->nhwc.W,
               pooled_height, pooled_width, 0, (float*)&boxes[4*i],
               output+roi_size*i);
